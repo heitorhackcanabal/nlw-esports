7a8fda668fd55d771bf062ebaa63a6bc95843673
Heitor Hack Canabal <heitorhc@ciandt.com>
2022-09-15 21:53:54 -0300

    Adicionando pasta mobile

---
+++ mobile/src/components/DuoMatch/index.tsx
@@ -0,0 +1,77 @@
+import { View, Modal, ModalProps, Text, TouchableOpacity, Alert, ActivityIndicator } from 'react-native';
+
+import { styles } from './styles';
+
+import { MaterialIcons } from '@expo/vector-icons'
+import { CheckCircle } from 'phosphor-react-native'
+import { THEME } from '../../theme';
+import { Heading } from '../Heading';
+
+import * as Clipboard from 'expo-clipboard'
+import { useState } from 'react';
+
+interface Props extends ModalProps {
+  discord: string;
+  onClose: () => void;
+}
+
+export function DuoMatch({discord, onClose, ...rest}: Props) {
+  const [isCopying, setIsCopying] = useState(false)
+
+  async function handleCopyDiscordToClipboard() {
+    setIsCopying(true);
+    await Clipboard.setStringAsync(discord);
+    
+    Alert.alert('Discord copiado!', 'Usuário copiado para a sua área de transferência.');
+    setIsCopying(false);
+  }
+
+  return (
+    <Modal
+      transparent
+      statusBarTranslucent
+      animationType='fade'
+      {...rest}
+    >
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <TouchableOpacity
+            style={styles.closeIcon}
+            onPress={onClose}
+          >
+            <MaterialIcons
+              name='close'
+              size={20}
+              color={THEME.COLORS.CAPTION_500}
+            />
+          </TouchableOpacity>
+
+          <CheckCircle
+            size={64}
+            color={THEME.COLORS.SUCCESS}
+            weight="bold"
+          />
+
+          <Heading
+            title="Let's Play"
+            subtitle='Agora é só começar a jogar!'
+            style={{ alignItems: 'center', marginTop: 24 }}
+          />
+
+          <Text style={styles.label}>
+            Adicione no Discord
+          </Text>
+          
+          <TouchableOpacity
+            style={styles.discordButton}
+            onPress={handleCopyDiscordToClipboard}
+            disabled={isCopying}>
+            <Text style={styles.discord}>
+              {isCopying ? <ActivityIndicator color={THEME.COLORS.PRIMARY} /> : discord}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+}
